a4452c79f7a23654ff1e53169a4a25a24d05be71
KevinKYeh <71478999+KevinKYeh@users.noreply.github.com>
2022-02-05 09:31:37 -0800

    Update script.js

--- v2/script.js
+++ v2/script.js
@@ -28,7 +28,7 @@ function escapeHtml(message) {
 }
 
 function TwitchAPI(url) {
-    return $.getJSON(url + (url.search(/\?/) > -1 ? '&' : '?') + 'client_id=' + client_id);
+    return $.getJSON(url + (url.search(/\?/) > -1 ? '&' : '?') + 'client_id=p31p8buadkrrel3fdzq7268cq6q7li');
 }
 
 Chat = {
