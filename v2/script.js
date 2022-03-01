ad0aa77acf8df7cf9d7e8057b5a3b50cfcd3d040
KevinKYeh <71478999+KevinKYeh@users.noreply.github.com>
2022-03-01 15:56:03 -0800

    Update script.js

--- v2/script.js
+++ v2/script.js
@@ -75,7 +75,7 @@ Chat = {
         cheers: {},
         lines: [],
         blockedUsers: ('block' in $.QueryString ? $.QueryString.block.toLowerCase().split(',') : false),
-        bots: ['streamelements', 'streamlabs', 'nightbot', 'moobot', 'fossabot'],
+        bots: ['streamelements', 'streamlabs', 'nightbot', 'moobot', 'fossabot', 'kyaybot', 'serybot'],
         nicknameColor: ('cN' in $.QueryString ? $.QueryString.cN : false)
     },
 
